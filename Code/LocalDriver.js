6f0b55dfa7d702ba515179d7319da0a14ed5674c
t-haumas <thethomasricks@gmail.com>
2024-05-08 01:19:03 -0600

    Closes #9

--- Code/LocalDriver.js
+++ Code/LocalDriver.js
@@ -6,15 +6,34 @@ import MinePath from "./MinePath.js";
 
 
 
-MinePath.readObtainablesObject();
+MinePath.init();
+
+// MinePath.addNewObtainable("crafting table", "milestone", "craft a crafting table");
+// MinePath.addNewObtainable("coal", "inventory", "mine coal");
+// MinePath.addNewObtainable("world: water source block", "inventory", "find a water source block");
+// MinePath.addNewObtainable("wooden pickaxe", "milestone", "craft a wooden pickaxe");
+
+MinePath.startNewAction("craft a crafting table");
+MinePath.addActionRequirement("wooden planks", "inventory", 4);
+MinePath.addActionYields("crafting table", "milestone", 1);
+MinePath.submitAction();
+
+// MinePath.startNewAction("mine coal");
+// MinePath.addActionRequirement("wooden pickaxe", "milestone", 1);
+// MinePath.addActionYields("coal", "inventory", 1);
+// MinePath.submitAction();
+
+// MinePath.startNewAction("find a water source block");
+// MinePath.addActionYields("world: water source block", "inventory", 1);
+// MinePath.submitAction();
+
+// MinePath.startNewAction("make an infinite water source");
+// MinePath.addActionRequirement("world: water source block", "inventory", 2);
+// MinePath.addActionRequirement("bucket", "milestone", 1);
+// MinePath.addActionYields("world: infinite water source", "milestone", 1);
+// MinePath.submitAction();
+
+MinePath.save();
 
 
-MinePath.startNewObtainable("stick", "inventory", "craft a stick", "crafting");
-MinePath.addObtainableRequirement("wooden plank", "inventory", 2, "crafting");
-MinePath.submitObtainable();
-
-
-
-MinePath.saveObtainablesObject();
-
-console.log(MinePath.getObtainablesObject());
+console.log(MinePath.getUnresolvedReferences("goal: a diamond and an obsidian"));
--- Code/MinePath.js
+++ Code/MinePath.js
@@ -12,10 +12,12 @@ const TYPE_STRINGS = ["inventory", "milestone"];
 
 // Global variables
 let obtainables = {};
+let actions = {};
 let initialized = false;
-let buildingObtainable = {};
+let buildingAction = {};
 let buildingRequirements = [];
-let buildingObtainableEntryString;
+let buildingYields = [];
+let buildingActionEntryString;
 
 // Functions
 function sayHellos() {
@@ -24,12 +26,9 @@ function sayHellos() {
     console.log(EditObtainables.getHello());
 }
 
-function saveObtainablesObject() {
-    FileIO.saveJSONToResourcesFile(obtainables, "obtainables.json");
-}
-
-function readObtainablesObject() {
+function init() {
     obtainables = JSON.parse(FileIO.readResourcesFileContent("obtainables.json"));
+    actions = JSON.parse(FileIO.readResourcesFileContent("actions.json"));
     initialized = true;
 }
 
@@ -37,39 +36,60 @@ function getObtainablesObject() {
     return obtainables;
 }
 
-function startNewObtainable(name, type, action, from) {
-    buildingObtainable["name"] = name;
-    buildingObtainable["type"] = type;
-    buildingObtainable["action"] = action;
-    buildingObtainable["requirements"] = [];
-    buildingObtainableEntryString = getObtainableEntryString(name, type, from);
+function getActionsObject() {
+    return actions;
 }
 
-function addObtainableRequirement(name, type, amount, from) {
-    //"name": "* required_obtainable_name <from method>",
-    // "type": "* inventory_OR_milestone",
-    // "amount": "* 1 for milestone, 1-infinity for inventory"
+function startNewAction(name) {
+    buildingActionEntryString = name;
+}
+
+function addNewObtainable(name, type, action) {
+    let obtainableEntryString = getObtainableEntryString(name, type);
+    obtainables[obtainableEntryString] = {};
+    obtainables[obtainableEntryString]["name"] = name;
+    obtainables[obtainableEntryString]["type"] = type;
+    obtainables[obtainableEntryString]["action"] = action;
+}
+
+function addActionRequirement(name, type, amount) {
     if (!isValidAmount(amount)) {
         throw "Invalid requirement amount: " + amount;
     }
 
     let newRequirement = {};
-    newRequirement["entry"] = getObtainableEntryString(name, type, from);
+    newRequirement["name"] = name;
+    newRequirement["type"] = type;
     newRequirement["amount"] = amount;
     buildingRequirements.push(newRequirement);
+}
+
+function addActionYields(name, type, amount) {
+    if (!isValidAmount(amount)) {
+        throw "Invalid requirement amount: " + amount;
+    }
+
+    let newYield = {};
+    newYield["name"] = name;
+    newYield["type"] = type;
+    newYield["amount"] = amount;
+    buildingYields.push(newYield);
 }
 
 /**
  * This will also update an obtainable that already exists at this key.
  */
-function submitObtainable() {
-    if (Object.keys(buildingObtainable).length === 0) {
+function submitAction() {
+    if (Object.keys(buildingRequirements).length === 0 && Object.keys(buildingYields).length === 0) {
         console.error("cannot submit an object that hasn't been built!");
     } else {
-        buildingObtainable["requirements"] = buildingRequirements;
-        obtainables[buildingObtainableEntryString] = buildingObtainable;
-        buildingObtainable = {};
+        buildingAction["requirements"] = buildingRequirements;
+        buildingAction["yields"] = buildingYields;
+        actions[buildingActionEntryString] = buildingAction;
+        buildingAction = {};
         buildingRequirements = [];
+        buildingYields = [];
+        buildingActionEntryString = undefined;
     }
 }
 
@@ -79,7 +99,7 @@ function getObtainableEntryString(name, type, from) {
     if (TYPE_STRINGS.includes(type)) {
         entryString += type + ": " + name;
         if (from !== undefined) {
-            entryString += " from " + from;
+            entryString += " from " + from; // From is not currently being used.
         }
     } else {
         throw "Bruh, you said '" + type + "' as a type, but it has to be one of these: " + TYPE_STRINGS + ".";
@@ -91,12 +111,61 @@ function isValidAmount(x) {
     return (typeof x === 'number' && Number.isInteger(x) && x >= 1);
 }
 
+let alreadyExplored = [];
+
+function getUnresolvedReferences(goal) {
+    let unresolvedObtainables = new Set();
+    let unresolvedActions = new Set();
+    if (!alreadyExplored.includes(goal)) {
+        alreadyExplored.push(goal);
+        if (goal in obtainables) {
+            let currentObtainable = obtainables[goal];
+            let currentAction = currentObtainable["action"];
+            if (currentAction in actions) {
+                let findings = getUnresolvedReferences(currentAction);
+                unresolvedActions = new Set([...unresolvedActions, ...findings["unresolved actions:"]]);
+                unresolvedObtainables = new Set([...unresolvedObtainables, ...findings["unresolved obtainables:"]]);
+            } else {
+                unresolvedActions.add(currentAction);
+            }
+        } else if (goal in actions) {
+            let currentAction = actions[goal];
+            let currentRequirements = currentAction["requirements"];
+            for (let i = 0; i < currentRequirements.length; i++) {
+                let currentRequirement = getObtainableEntryString(currentRequirements[i]["name"], currentRequirements[i]["type"]);
+                if (currentRequirement in obtainables) {
+                    let findings = getUnresolvedReferences(currentRequirement);
+                    unresolvedActions = new Set([...unresolvedActions, ...findings["unresolved actions:"]]);
+                    unresolvedObtainables = new Set([...unresolvedObtainables, ...findings["unresolved obtainables:"]]);
+                } else {
+                    unresolvedObtainables.add({ name: currentRequirements[i]["name"], type: currentRequirements[i]["type"] });
+                }
+            }
+
+            // Can also copy above block for yields.
+
+        } else {
+            throw goal + " is not an entry in the obtainables or actions list.";
+        }
+    }
+    return {"unresolved actions:" : unresolvedActions, "unresolved obtainables:" : unresolvedObtainables};
+}
+
+function save() {
+    FileIO.saveJSONToResourcesFile(obtainables, "obtainables.json");
+    FileIO.saveJSONToResourcesFile(actions, "actions.json");
+}
+
 export default {
     sayHellos,
-    saveObtainablesObject,
-    readObtainablesObject,
     getObtainablesObject,
-    startNewObtainable,
-    addObtainableRequirement,
-    submitObtainable
+    addNewObtainable,
+    startNewAction,
+    addActionRequirement,
+    addActionYields,
+    submitAction,
+    getUnresolvedReferences,
+    init,
+    getActionsObject,
+    save
 };
